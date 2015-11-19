f16c5020a1a938f1f8332d2b6cae917bd73863f5
Dawn Carroll <dsc254@gmail.com>
2015-11-19 12:01:59 +0000

    Update Day3Exercise2.java

--- Day3Exercise2.java
+++ Day3Exercise2.java
@@ -1,10 +1,16 @@
-class HeightWeightDepth {
+class Measurmentings {
 	
-	private int height;
-	private int weight;
-	private int depth;
+	int height;
+	int weight;
+	int depth;
 	
-	public HeightWeightDepth(int height, int weight, int depth) {
+	public Measurmentings() {
+		this.height = 0;
+		this.weight = 0;
+		this.depth = 0;
+	}
+	
+	public Measurmentings(int height, int weight, int depth) {
 		this.height = height;
 		this.weight = weight;
 		this.depth = depth;
@@ -27,11 +33,25 @@ class HeightWeightDepth {
 public class Day3Exercise2 {
 	public static void main(String[] args) {
 		
-		HeightWeightDepth building = new HeightWeightDepth(50,300,5);
+		Measurmentings building1 = new Measurmentings(50,300,5);
+		Measurmentings building2 = new Measurmentings();
 		
-		System.out.println("The building's height is " + building.getHeight() + " feet.");
-		System.out.println("The building's weight is " + building.getWeight() + " pounds.");
-		System.out.println("The building's depth is " + building.getDepth() + " feet.");
+		System.out.println("Building 1's height is " + building1.getHeight() + " feet.");
+		System.out.println("Building 1's weight is " + building1.getWeight() + " pounds.");
+		System.out.println("Building 1's depth is " + building1.getDepth() + " feet.");
+		System.out.println("\nBuilding 2's height is " + building2.getHeight() + " feet.");
+		System.out.println("Building 2's weight is " + building2.getWeight() + " pounds.");
+		System.out.println("Building 2's depth is " + building2.getDepth() + " feet.");
+		System.out.println("\nOops! Forgot to add Building 2's data!");
+		
+		building2.height = 60;
+		building2.weight = 445;
+		building2.depth = 16;
+		
+		System.out.println("\nBuilding 2's height is " + building2.getHeight() + " feet.");
+		System.out.println("Building 2's weight is " + building2.getWeight() + " pounds.");
+		System.out.println("Building 2's depth is " + building2.getDepth() + " feet.");
+		
 		
 		
 		
