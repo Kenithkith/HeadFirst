5f35f09c051d79cdb4adcd586314513a0349ed3b
Dawn Carroll <dsc254@gmail.com>
2015-12-03 16:18:41 +0000

    Update Day4Exercise2.java

--- Day4Exercise2.java
+++ Day4Exercise2.java
@@ -1,22 +1,26 @@
+import java.util.Scanner;
+
 // Create a class that takes words for the first ten numbers 
 // ("one" to "ten") and converts them into a single LONG integer.
 // Use a switch statement for the conversion and command-line arguments
 // for the words.  (Or use scanner if you want it look better.)
 
-
 public class Day4Exercise2 {
 	public static void main(String[] args) {
 
 		long number = 0;
-		String word = "six";
-		
-		if (args.length > 0)
+		String word;
+
+		if (args.length > 0) 
 			word = new String(args[0]);
 		
-		char[] starter = word.toCharArray();
-			
-	
-		switch (starter[0]) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number from one to ten, in word format.");
+		word = input.nextLine();
+
+			char[] starter = word.toCharArray();
+
+			switch (starter[0]) {
 			case 'o':
 				number = 1l;
 				break;
@@ -27,37 +31,35 @@ public class Day4Exercise2 {
 					number = 3l;
 				if (starter[1] == 'e')
 					number = 10l;
-					break;
+				break;
 			case 'f':
 				if (starter[1] == 'o')
 					number = 4l;
 				if (starter[1] == 'i')
 					number = 5l;
-					break;
+				break;
 			case 's':
 				if (starter[1] == 'i')
 					number = 6l;
 				if (starter[1] == 'e')
 					number = 7l;
-					break;
+				break;
 			case 'e':
 				number = 8l;
 				break;
 			case 'n':
 				number = 9l;
 				break;
-				
+
 			default:
 				System.out.println("Number input error.");
-				
-			//return number;
-				
-		}
+
+				// return number;
+
+			}
+
+			System.out.println("Your number is " + number + ".");
 		
-		System.out.println("Your number is " + number + ".");
-		
+	input.close();
 	}
 }
-		
-
-
