805e068fd12bc687c8ebaba3e2789508c03bc055
Dawn Carroll <dsc254@gmail.com>
2015-12-15 11:59:16 +0000

    Create PrinterExample.java

---
+++ PrinterExample.java
@@ -0,0 +1,24 @@
+
+public class PrinterExample {
+
+	int x = 0;
+	int y = 1;
+
+	void printMe() {
+		System.out.println("x is " + x + " and y is " + y + ".");
+		System.out.println("I am an instance of the class "
+				+ this.getClass().getName() + ".");
+	}
+	
+}
+
+class SubPrinter extends PrinterExample {
+	
+	int z = 3;
+	
+	public static void main(String[] args) {
+		SubPrinter obj = new SubPrinter();
+		obj.printMe();
+	}
+	
+}
